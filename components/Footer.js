d5c051ea62c68603c880046d40d5e87aa2ecb60e
Spoonska <Spencer.Kyle@hotmail.com>
2023-06-17 09:45:59 -0600

    added png logo

--- components/Footer.js
+++ components/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
                     <Center>
                     <VStack spacing={4}>
                     <Image 
-                        src='/logo-trans-2.svg'
+                        src='/logo-trans-2.png'
                         height={200}
                         width={200}
                         alt='wandering jade logo'
