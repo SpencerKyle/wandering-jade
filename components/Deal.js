69b4af7872abcc92e175002711dffc1e243b3437
Spoonska <Spencer.Kyle@hotmail.com>
2023-06-19 15:57:50 -0600

    changed deals color

--- components/Deal.js
+++ components/Deal.js
@@ -3,7 +3,7 @@ import { Heading } from '@chakra-ui/react'
 
 function Deal() {
     return (
-        <Heading align='center' bgColor='#5c7551' p='3' top={0} width='100%' zIndex={10000} mt={['14', '0', '0']}>Grand Opening Sale! $100 off all July bookings!</Heading>
+        <Heading align='center' bgColor='#5c7551' color='gray.200' p='3' top={0} width='100%' zIndex={10000} mt={['14', '0', '0']}>Grand Opening Sale! $100 off all July bookings!</Heading>
     )
 }
 
