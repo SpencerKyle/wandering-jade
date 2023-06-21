03d688d5fdad54fff8f3f09c04fb32e3746a4e5b
Spoonska <Spencer.Kyle@hotmail.com>
2023-06-21 15:33:40 -0600

    changed wording on July Banner

--- components/Deal.js
+++ components/Deal.js
@@ -3,7 +3,7 @@ import { Heading } from '@chakra-ui/react'
 
 function Deal() {
     return (
-        <Heading fontFamily='banner' align='center' bgColor='#5c7551' color='gray.200' p='3' top={0} width='100%' zIndex={10000} mt={['14', '0', '0']}>Grand Opening Sale! $100 off all July bookings!</Heading>
+        <Heading fontFamily='banner' align='center' bgColor='#5c7551' color='white' p='3' size={['md',null ,'lg']} top={0} width='100%' zIndex={10000} mt={['14', '0', '0']}>Grand Opening Sale! $100 off when you put a deposit down in July</Heading>
     )
 }
 
